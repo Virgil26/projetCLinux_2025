52887e99dbda3e5e75eed9e91491bf39c3a082cb
Virgil <virgilm_@hotmail.com>
2026-09-06 13:45:32 +0200

    :sparkles: Etape 4 (Publicite) : lecture en boucle des publicites
    - Recuperation de la file de messages et de la memoire partagee
      creees par le Serveur, puis attachement a la memoire partagee
    - Boucle infinie : lecture d'un enregistrement PUBLICITE dans
      publicites.dat (retour au debut du fichier une fois la fin
      atteinte), copie du texte en memoire partagee, envoi de UPDATE_PUB
      au Serveur, puis attente du nombre de secondes prevu

--- Publicite.cpp
+++ Publicite.cpp
@@ -27,22 +27,72 @@ int main()
 
   // Recuperation de l'identifiant de la file de messages
   fprintf(stderr,"(PUBLICITE %d) Recuperation de l'id de la file de messages\n",getpid());
+  
+  // *** ETAPE 4 : AJOUT ***
+  if((idQ = msgget(CLE, 0)) == -1)
+  {
+    perror("(PUBLICITE) Erreur de msgget");
+    exit(1);
+  }
 
   // Recuperation de l'identifiant de la mémoire partagée
   fprintf(stderr,"(PUBLICITE %d) Recuperation de l'id de la mémoire partagée\n",getpid());
+  
+  // *** ETAPE 4 : AJOUT ***
+  if((idShm = shmget(CLE, 200, 0)) == -1)
+  {
+    perror("(PUBLICITE) Erreur de shmget");
+    exit(1);
+  }
 
   // Attachement à la mémoire partagée
+  // *** ETAPE 4 : AJOUT ***
+  char* zonePartagee = (char*) shmat(idShm,NULL,0); 
+  if (zonePartagee == (char*) -1)
+  {
+    perror("(PUBLICITE) Erreur de shmat");
+    exit(1);
+  }
 
   // Ouverture du fichier de publicité
-
+  // *** ETAPE 4 : AJOUT ***
+  fd = open("publicites.dat",O_RDONLY);          
+  if (fd == -1)
+  {
+    perror("(PUBLICITE) Erreur d'ouverture de publicites.dat");
+    exit(1);
+  }
   while(1)
   {
-  	PUBLICITE pub;
+    PUBLICITE pub;
+
     // Lecture d'une publicité dans le fichier
+    // *** ETAPE 4 : AJOUT ***
+    int n = read(fd,&pub,sizeof(PUBLICITE));        
+    if (n == 0)    // fin de fichier : on repart au debut
+    {
+      lseek(fd,0,SEEK_SET);
+      n = read(fd,&pub,sizeof(PUBLICITE));
+    }
+    if (n != sizeof(PUBLICITE))
+    {
+      perror("(PUBLICITE) Erreur de lecture de publicites.dat");
+      exit(1);
+    }
 
     // Ecriture en mémoire partagée
+    // *** ETAPE 4 : AJOUT ***
+    strcpy(zonePartagee,pub.texte);
 
     // Envoi d'une requete UPDATE_PUB au serveur
+    // *** ETAPE 4 : AJOUT ***
+    MESSAGE m;
+    m.type = 1; // 1 = destination "Serveur"
+    m.expediteur = getpid();
+    m.requete = UPDATE_PUB;
+    msgsnd(idQ,&m,sizeof(MESSAGE)-sizeof(long),0);
+
+    sleep(pub.nbSecondes); // ETAPE 4 - AJOUT : attente avant la pub suivante
 
   }
 }
